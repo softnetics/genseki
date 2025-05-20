b5970682b55daaa24b47969eb9566a7e4b734c39
supakornnetsuwan <earth_killerdark@hotmail.com>
2025-05-20 12:10:44 +0700

    chore: remove unused component

--- packages/next/src/components/delete-button.tsx
+++
@@ -1,7 +0,0 @@
-import { ServerConfig } from '~/core/config'
-
-export function DeleteButton<TServerConfig extends ServerConfig>(props: {
-  serverConfig: TServerConfig
-}) {
-  return <button>Delete</button>
-}
