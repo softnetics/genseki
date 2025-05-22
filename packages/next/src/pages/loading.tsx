75df96867443244720a55167571b7d2bf09fbec9
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2025-05-22 12:21:07 +0700

    chore: fix named export

--- packages/next/src/pages/loading.tsx
+++ packages/next/src/pages/loading.tsx
@@ -2,7 +2,7 @@ import { SpinnerGapIcon } from '@phosphor-icons/react/dist/ssr'
 
 import { BaseIcon } from '../components/primitives/base-icon'
 
-const LoadingPage = () => {
+export const LoadingPage = () => {
   return (
     <div className="from-bg to-primary/10 absolute inset-0 grid h-dvh content-center justify-center bg-gradient-to-br from-50% px-20">
       <BaseIcon
@@ -14,5 +14,3 @@ const LoadingPage = () => {
     </div>
   )
 }
-
-export default LoadingPage
