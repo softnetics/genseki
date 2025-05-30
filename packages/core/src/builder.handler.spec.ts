35b5e3c0c4a439e365453a805230115c6e803787
miello <partearth1212@gmail.com>
2025-05-31 00:58:51 +0700

    fix: unmatch db name causes error when lookup table

--- packages/core/src/builder.handler.spec.ts
+++ packages/core/src/builder.handler.spec.ts
@@ -531,6 +531,7 @@ describe('ApiHandler', () => {
             idTs: postData.id,
             nameTs: postData.name,
             authorTs: {
+              __pk: authorData.id,
               idTs: authorData.id,
               nameTs: authorData.name,
             },
@@ -554,6 +555,9 @@ describe('ApiHandler', () => {
                   columns: {
                     idTs: true,
                     nameTs: true,
+                  },
+                  extras: {
+                    __pk: sql<string | number>`${schema.authorTs.idTs}`.as('__pk'),
                   },
                   with: {},
                 },
@@ -758,6 +762,7 @@ describe('ApiHandler', () => {
             nameTs: authorData.name,
             postsTs: [
               {
+                __pk: postData.id,
                 idTs: postData.id,
                 nameTs: postData.name,
               },
@@ -781,6 +786,9 @@ describe('ApiHandler', () => {
                 columns: {
                   idTs: true,
                   nameTs: true,
+                },
+                extras: {
+                  __pk: sql<string | number>`${schema.postWithAuthorTs.idTs}`.as('__pk'),
                 },
                 with: {},
               },
@@ -1039,10 +1047,17 @@ describe('ApiHandler', () => {
                     idTs: true,
                     nameTs: true,
                   },
+                  extras: {
+                    __pk: sql<string | number>`${schema.authorTs.idTs}`.as('__pk'),
+                  },
                   with: {},
                 },
               },
               where: eq(postWithAuthorConnectCollection.fields.idField._.column, postData.id),
+              extras: {
+                __pk: sql<string | number>`${schema.postWithAuthorTs.idTs}`.as('__pk'),
+                __id: sql<string | number>`${schema.postWithAuthorTs.idTs}`.as('__id'),
+              },
             })
           )
 
@@ -1242,19 +1257,16 @@ describe('ApiHandler', () => {
             postsTs: [
               {
                 __pk: mockPostData[0].id,
-                __id: mockPostData[0].id,
                 idTs: mockPostData[0].id,
                 nameTs: mockPostData[0].name,
               },
               {
                 __pk: mockPostData[1].id,
-                __id: mockPostData[1].id,
                 idTs: mockPostData[1].id,
                 nameTs: mockPostData[1].name,
               },
               {
                 __pk: mockPostData[2].id,
-                __id: mockPostData[2].id,
                 idTs: mockPostData[2].id,
                 nameTs: mockPostData[2].name,
               },
@@ -1278,6 +1290,9 @@ describe('ApiHandler', () => {
                 columns: {
                   idTs: true,
                   nameTs: true,
+                },
+                extras: {
+                  __pk: sql<string | number>`${schema.postWithAuthorTs.idTs}`.as('__pk'),
                 },
                 with: {},
               },
@@ -1713,7 +1728,6 @@ describe('ApiHandler', () => {
               nameTs: postData.name,
               authorTs: {
                 __pk: authorData.id,
-                __id: authorData.id,
                 idTs: authorData.id,
                 nameTs: authorData.name,
               },
