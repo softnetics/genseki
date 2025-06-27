fc6e2ceae8e84cff71589397c598aa375fa8606f
intaniger <intaniger@gmail.com>
2025-06-28 02:33:08 +0700

    test: :white_check_mark: assertType test: WhereExpression over a number of difference field types

--- packages/react/src/core/__mocks__/vary-collection-server-config.ts
+++ packages/react/src/core/__mocks__/vary-collection-server-config.ts
@@ -1,8 +1,19 @@
+import Color from '@tiptap/extension-color'
+import TextAlign from '@tiptap/extension-text-align'
+import TextStyle from '@tiptap/extension-text-style'
+import Underline from '@tiptap/extension-underline'
+import StarterKit from '@tiptap/starter-kit'
 import { drizzle } from 'drizzle-orm/node-postgres'
 
 import { allFieldTypes } from './all-type-schema'
 import * as baseSchema from './complex-schema'
 
+import {
+  BackColorExtension,
+  CustomImageExtension,
+  ImageUploadNodeExtension,
+  SelectionExtension,
+} from '../../react'
 import { Builder } from '../builder'
 import { defineBaseConfig, defineServerConfig } from '../config'
 
@@ -14,6 +25,44 @@ const db = drizzle({
   connection: '',
   schema: schema,
 })
+
+export const postEditorProviderProps = {
+  immediatelyRender: false,
+  shouldRerenderOnTransaction: true,
+  content: '<h2>This came from Post content field</h2>',
+  extensions: [
+    Color,
+    BackColorExtension,
+    Underline.configure({ HTMLAttributes: { class: 'earth-underline' } }),
+    SelectionExtension,
+    TextStyle,
+    TextAlign.configure({
+      types: ['heading', 'paragraph'],
+      alignments: ['left', 'center', 'right', 'justify'],
+      defaultAlignment: 'left',
+    }),
+    StarterKit.configure({
+      bold: { HTMLAttributes: { class: 'bold large-black' } },
+      paragraph: { HTMLAttributes: { class: 'paragraph-custom' } },
+      heading: { HTMLAttributes: { class: 'heading-custom' } },
+      bulletList: { HTMLAttributes: { class: 'list-custom' } },
+      orderedList: { HTMLAttributes: { class: 'ordered-list' } },
+      code: { HTMLAttributes: { class: 'code' } },
+      codeBlock: { HTMLAttributes: { class: 'code-block' } },
+      horizontalRule: { HTMLAttributes: { class: 'hr-custom' } },
+      italic: { HTMLAttributes: { class: 'italic-text' } },
+      strike: { HTMLAttributes: { class: 'strikethrough' } },
+      blockquote: { HTMLAttributes: { class: 'blockquote-custom' } },
+    }),
+    CustomImageExtension.configure({ HTMLAttributes: { className: 'image-displayer' } }),
+    ImageUploadNodeExtension.configure({
+      showProgress: false,
+      accept: 'image/*',
+      maxSize: 1024 * 1024 * 10, // 10MB
+      limit: 3,
+    }),
+  ],
+}
 
 export const baseConfig = defineBaseConfig({
   db: db,
@@ -64,6 +113,70 @@ const vary = builder.collection('vary', {
     text_enum: fb.columns('textEnum', { type: 'text' }),
     varchar: fb.columns('varchar', { type: 'text' }),
     char: fb.columns('char', { type: 'text' }),
+    // numeric: fb.columns('numeric', { type: 'text' }), // Not supported yet
+    // decimal: fb.columns('decimal', { type: 'text' }), // Not supported yet
+    real: fb.columns('real', { type: 'number' }),
+    doublePrecision: fb.columns('doublePrecision', { type: 'number' }),
+    json: fb.columns('json', { type: 'richText', editor: postEditorProviderProps }), // Not supported yet
+    jsonType: fb.columns('jsonType', { type: 'richText', editor: postEditorProviderProps }), // Not supported yet
+    jsonb: fb.columns('jsonb', { type: 'richText', editor: postEditorProviderProps }), // Not supported yet
+    jsonbType: fb.columns('jsonbType', { type: 'richText', editor: postEditorProviderProps }), // Not supported yet
+    time: fb.columns('time', { type: 'time' }),
+    timestamp: fb.columns('timestamp', { type: 'date' }),
+    date: fb.columns('date', { type: 'date' }),
+    // interval: fb.columns('interval', { type: 'time' }), // Not supported yet
+    // point: fb.columns('point', { type: 'comboboxNumber' }), // Not supported yet
+    // line: fb.columns('line', { type: 'comboboxNumber' }), // Not supported yet
+    // enum: fb.columns('enum', { type: 'comboboxNumber' }), // Not supported yet
+
+    arrayOfText: fb.columns('arrayOfText', {
+      type: 'comboboxText',
+      options: () => [{ label: '', value: '' }],
+    }),
+    arrayOfNumbers: fb.columns('arrayOfNumbers', {
+      type: 'comboboxNumber',
+      options: () => [{ label: '', value: 0 }],
+    }),
+    // arrayOfObjects: fb.columns('arrayOfObjects', {
+    //   type: 'comboboxText',
+    //   options: () => [{ label: '', value: '' }],
+    // }), Not supported yet
+    arrayOfBooleans: fb.columns('arrayOfBooleans', {
+      type: 'comboboxBoolean',
+      options: () => [{ label: '', value: false }],
+    }),
+    arrayOfDates: fb.columns('arrayOfDates', {
+      type: 'comboboxText',
+      options: () => [{ label: '', value: '' }],
+    }),
+    arrayOfTimes: fb.columns('arrayOfTimes', {
+      type: 'comboboxText',
+      options: () => [{ label: '', value: '' }],
+    }),
+    // arrayOfTimestamps: fb.columns('arrayOfTimestamps', {
+    //   type: '',
+    //   options: () => [{ label: '', value: '' }],
+    // }),// Not supported yet
+    // arrayOfPoints: fb.columns('arrayOfPoints', {
+    //   type: '',
+    //   options: () => [{ label: '', value: '' }],
+    // }), // Not supported yet
+    // arrayOfLines: fb.columns('arrayOfLines', {
+    //   type: '',
+    //   options: () => [{ label: '', value: '' }],
+    // }), // Not supported yet
+    arrayOfEnums: fb.columns('arrayOfEnums', {
+      type: 'comboboxText',
+      options: () => [{ label: '', value: '' }],
+    }),
+    // arrayOfJson: fb.columns('arrayOfJson', {
+    //   type: '',
+    //   options: () => [{ label: '', value: '' }],
+    // }), // Not supported yet
+    // arrayOfJsonb: fb.columns('arrayOfJsonb', {
+    //   type: '',
+    //   options: () => [{ label: '', value: '' }],
+    // }), // Not supported yet
   })),
 })
 export const serverConfig = defineServerConfig(baseConfig, {
--- packages/react/src/core/filter.spec.ts
+++ packages/react/src/core/filter.spec.ts
@@ -1,18 +1,176 @@
-import { assertType, test } from 'vitest'
+import { assertType, describe, it } from 'vitest'
 
 import type { VaryCollectionFields } from './__mocks__/vary-collection-server-config'
 import type { WhereExpression } from './filter'
 
-test('assertType: WhereExpression', () => {
-  assertType<WhereExpression<VaryCollectionFields>>({
-    $and: [{ integer: { $eq: 1 } }, { $or: [{ boolean: '$isNotNull' }] }],
-    bigserialNumber: { $eq: 1 },
+type VaryCollectionWhereExpr = WhereExpression<VaryCollectionFields>
 
-    integer: { $between: [1, 2] },
-    text: { $ilike: '23' },
-    boolean: { $eq: true },
-    char: { $like: '' },
-    textEnum: { $inArray: [''] },
-    varchar: { $ilike: '' },
+describe('assertType: WhereExpression', () => {
+  it('For numeric fields, it should support CompareOperators, NullOperators, and ListOperator', () => {
+    // CompareOperators
+    assertType<VaryCollectionWhereExpr>({
+      smallint: { $gt: 1 },
+      integer: { $gte: 1 },
+      serial: { $lt: 2 },
+      smallserial: { $lte: 2 },
+      bigserialNumber: { $between: [1, 2] },
+      real: { $notBetween: [1, 2] },
+      doublePrecision: { $gt: 1 },
+    })
+
+    // NullOperators
+    assertType<VaryCollectionWhereExpr>({
+      smallint: '$isNotNull',
+      integer: '$isNull',
+      serial: '$isNotNull',
+      smallserial: '$isNull',
+      bigserialNumber: '$isNotNull',
+      real: '$isNull',
+      doublePrecision: '$isNotNull',
+    })
+
+    // ListOperator
+    assertType<VaryCollectionWhereExpr>({
+      smallint: {
+        $inArray: [1, 2, 3],
+      },
+      integer: {
+        $notInArray: [1, 2, 3],
+      },
+      serial: {
+        $inArray: [1, 2, 3],
+      },
+      smallserial: {
+        $notInArray: [1, 2, 3],
+      },
+      bigserialNumber: {
+        $inArray: [1, 2, 3],
+      },
+      real: {
+        $notInArray: [1, 2, 3],
+      },
+      doublePrecision: {
+        $inArray: [1, 2, 3],
+      },
+    })
+  })
+
+  it('For string fields, it should support StringOperators, NullOperators, EqualityOperator, ListOperator', () => {
+    // StringOperators
+    assertType<VaryCollectionWhereExpr>({
+      text: { $like: '%' },
+      textEnum: { $ilike: '%' },
+      varchar: { $notIlike: '%' },
+      char: { $like: '%' },
+    })
+
+    // NullOperators
+    assertType<VaryCollectionWhereExpr>({
+      text: '$isNotNull',
+      textEnum: '$isNull',
+      varchar: '$isNotNull',
+      char: '$isNull',
+    })
+
+    // EqualityOperator
+    assertType<VaryCollectionWhereExpr>({
+      text: { $eq: 'text' },
+      textEnum: { $eq: 'textEnum' },
+      varchar: { $eq: 'varchar' },
+      char: { $eq: 'char' },
+    })
+
+    // ListOperator
+    assertType<VaryCollectionWhereExpr>({
+      text: { $inArray: ['text', 'textEnum', 'varchar', 'char'] },
+      textEnum: { $notInArray: ['text', 'textEnum', 'varchar', 'char'] },
+      varchar: { $inArray: ['text', 'textEnum', 'varchar', 'char'] },
+      char: { $notInArray: ['text', 'textEnum', 'varchar', 'char'] },
+    })
+  })
+
+  it('For boolean fields, it should support NullOperators, EqualityOperator', () => {
+    // EqualityOperator
+    assertType<VaryCollectionWhereExpr>({
+      boolean: { $eq: true },
+    })
+    assertType<VaryCollectionWhereExpr>({
+      boolean: { $ne: false },
+    })
+
+    // NullOperators
+    assertType<VaryCollectionWhereExpr>({
+      boolean: '$isNotNull',
+    })
+    assertType<VaryCollectionWhereExpr>({
+      boolean: '$isNull',
+    })
+  })
+
+  it('For JSON, JSONB, and Array fields, it should support ArrayOperators, NullOperators, and ListOperator', () => {
+    // ArrayOperators
+    assertType<VaryCollectionWhereExpr>({
+      json: { $arrayContained: [1, '2'] },
+      jsonb: { $arrayContains: [2, true] },
+      jsonType: { $arrayOverlaps: [3, 5, null] },
+      jsonbType: { $arrayContained: [false, 3] },
+      arrayOfBooleans: { $arrayContained: [1, '2'] },
+      arrayOfDates: { $arrayContains: [2, true] },
+      arrayOfEnums: { $arrayOverlaps: [3, 5, null] },
+      arrayOfNumbers: { $arrayContained: [1, '2'] },
+      arrayOfText: { $arrayContains: [2, true] },
+      arrayOfTimes: { $arrayOverlaps: [3, 5, null] },
+    })
+
+    // NullOperators
+    assertType<VaryCollectionWhereExpr>({
+      json: '$isNull',
+      jsonb: '$isNotNull',
+      jsonType: '$isNull',
+      jsonbType: '$isNotNull',
+      arrayOfBooleans: '$isNull',
+      arrayOfDates: '$isNotNull',
+      arrayOfEnums: '$isNull',
+      arrayOfNumbers: '$isNull',
+      arrayOfText: '$isNotNull',
+      arrayOfTimes: '$isNull',
+    })
+
+    // ListOperator
+    assertType<VaryCollectionWhereExpr>({
+      json: { $inArray: [1, false] },
+      jsonb: { $notInArray: [0, true] },
+      jsonType: { $inArray: [1, false] },
+      jsonbType: { $notInArray: [0, true] },
+      arrayOfBooleans: { $inArray: [1, false] },
+      arrayOfDates: { $notInArray: [0, true] },
+      arrayOfEnums: { $inArray: [1, false] },
+      arrayOfNumbers: { $notInArray: [1, false] },
+      arrayOfText: { $inArray: [0, true] },
+      arrayOfTimes: { $notInArray: [1, false] },
+    })
+  })
+
+  it('For date, time, datetime fields, it should support CompareOperators, NullOperators, and ListOperator', () => {
+    // CompareOperators
+    assertType<VaryCollectionWhereExpr>({
+      time: { $between: [new Date(), new Date()] },
+      timestamp: { $gt: new Date() },
+      date: { $gte: new Date() },
+    })
+
+    // NullOperators
+    assertType<VaryCollectionWhereExpr>({
+      time: { $eq: new Date() },
+      timestamp: { $ne: new Date() },
+      date: { $eq: new Date() },
+    })
+
+    // ListOperator
+    assertType<VaryCollectionWhereExpr>({
+      time: { $inArray: [new Date()] },
+      timestamp: { $notInArray: [new Date()] },
+      date: { $inArray: [new Date()] },
+    })
   })
 })
