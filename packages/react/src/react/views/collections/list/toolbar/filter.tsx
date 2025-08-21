96b1272b59c72cc53307db075876e97baf9292fe
pondc <chinnakrit.pond@gmail.com>
2025-08-21 18:51:25 +0700

    fix: wrong search handling

--- packages/react/src/react/views/collections/list/toolbar/filter.tsx
+++ packages/react/src/react/views/collections/list/toolbar/filter.tsx
@@ -2,16 +2,23 @@
 import { FunnelIcon } from '@phosphor-icons/react'
 
 import { BaseIcon, Button, type ButtonProps } from '../../../../components'
+import { useFilter } from '../../../../hooks/use-filter'
 
 export interface MinimalCollectionListFilterProps {
   isLoading?: boolean
-  onFilterChange?: (value: string) => void
+  onFilterChange?: (value: string) => void // Optional function to handle filter changes
 }
 export interface CollectionListFilterProps
   extends MinimalCollectionListFilterProps,
     Partial<ButtonProps> {}
 
+/**
+ * @param props.onFilterChange An event handler for search value change
+ * @param props.isLoading A loading state
+ */
 export function CollectionListFilter(props: CollectionListFilterProps) {
+  const { setFilter } = useFilter({ debounce: 500 })
+
   const createRandomText = () => {
     const random0to5 = (): number => Math.floor(Math.random() * 6)
     const arr = [
@@ -31,8 +38,11 @@ export function CollectionListFilter(props: CollectionListFilterProps) {
       variant="outline"
       size="md"
       onClick={() => {
-        // Random some text
-        props.onFilterChange?.(createRandomText())
+        // Open a modal or something to select filter options
+        // Implement logic over there, not here
+        props.onFilterChange
+          ? props.onFilterChange(createRandomText())
+          : setFilter(createRandomText())
       }}
       leadingIcon={<BaseIcon icon={FunnelIcon} size="md" />}
       isPending={props.isLoading}
