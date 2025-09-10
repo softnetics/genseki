706a717bbdbe7faaa3885e035168966330c6ecc2
jettapat <jettapat@metierthailand.com>
2025-09-10 18:19:28 +0700

    fix: remove unused hook and

--- packages/react/src/react/views/collections/list/default.tsx
+++ packages/react/src/react/views/collections/list/default.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import { useMemo } from 'react'
-
 import { Banner } from './banner'
 import { CollectionListTableContainer } from './container'
 import { useCollectionList } from './context'
@@ -12,17 +10,12 @@ import { CollectionListToolbar } from './toolbar'
 export function DefaultCollectionListPage() {
   const context = useCollectionList()
 
-  const columns = useMemo(() => {
-    if (context.isQuerying) return context.columns
-
-    return context.columns
-  }, [context.columns, context.isQuerying])
   return (
     <>
       <Banner slug={context.slug} />
       <CollectionListTableContainer>
         <CollectionListToolbar />
-        <CollectionListTable<any> columns={columns} onRowClick={undefined} />
+        <CollectionListTable<any> columns={context.columns} onRowClick={undefined} />
         <CollectionListPagination />
       </CollectionListTableContainer>
     </>
